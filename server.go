74594afe6c37e8d78dc116f79bdff2a0095c20b9
Allan Muhari <allanmuhari087@gmail.com>
2023-02-07 00:31:43 +0300

    fixed a post request error

--- server.go
+++ server.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"gitlab.com/golang-gin-po/controller"
+	"gitlab.com/golang-gin-po/middleware"
+	"gitlab.com/golang-gin-po/service"
 )
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
 
 func main() {
 
-	server := gin.Default()
+	setupLogOutput()
 
-	server.GET("/test", func(Context *gin.Context) {
-		Context.JSON(200, gin.H{
-			"message ": "OK !!",
-		})
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middleware.Logger())
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
 	})
 
-	server.Run(":8080")
 }
