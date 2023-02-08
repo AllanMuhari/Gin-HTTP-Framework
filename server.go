e636c75bcedfb076a7c3f7aaee1721e45dd55172
Allan Muhari <allanmuhari087@gmail.com>
2023-02-08 16:55:05 +0300

    created  apiroutes

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -25,15 +26,31 @@ func main() {
 	setupLogOutput()
 
 	server := gin.New()
+	server.Static("/css", "./templates/css")
+
+	server.LoadHTMLGlob("templates/*.html")
 
 	server.Use(gin.Recovery(), middleware.Logger(),
 		middleware.BasicAuth())
 
-	server.GET("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.FindAll())
-	})
-	server.POST("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.Save(ctx))
-	})
+	apiRoutes := server.Group("/api")
+	{
+		apiRoutes.GET("/videos", func(ctx *gin.Context) {
+			ctx.JSON(200, videoController.FindAll())
+		})
+		apiRoutes.POST("/videos", func(ctx *gin.Context) {
+			err := videoController.Save(ctx)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{"message": "Video input is valid."})
+			}
+		})
+	}
+	viewRoutes := server.Group("/view")
+	{
+		viewRoutes.GET("/videos", videoController.ShowAll)
+	}
+
 	server.Run(":8080")
 }
