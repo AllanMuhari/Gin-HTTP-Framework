3415dbef1f57802d74c7ac3076b50bfded36ab51
Allan Muhari <allanmuhari087@gmail.com>
2023-02-28 18:30:24 +0300

    authentication

---
+++ controller/login_controller.go
@@ -0,0 +1,18 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+
+
+func (controller *loginController) Login(ctx *gin.Context) string{
+	var credentials dto.Credentials
+	err := ctx.ShouldBind($credentials)
+    if err != nil{
+		return ""
+	}
+	isAuthenticated := controller.loginService.Login(credentials.Username, credentials.Password)
+	if isAuthenticated{
+		return controller.jwtService.GenerateToken(credentials.Username, true)
+	}
+	return ""
+}
